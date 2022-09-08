50bd0c3fa2fd06e6c69aab9152d9d5d63db18dee
João E. Pereira Jr <joaoepj@gmail.com>
2022-09-08 16:35:16 -0300

    ASN.1 library test

---
+++ old_files_to_organize/asn1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/asn1"
+	"fmt"
+	"time"
+)
+
+func main() {
+	//t1, _ := time.Parse("20220904131020", "20220904131020")
+	t1 := time.Now()
+	var t2 time.Time
+	b := []byte("20220904131020")
+	mdata, _ := asn1.Marshal(t1)
+	fmt.Println(mdata, b)
+	_, err := asn1.Unmarshal(mdata, &t2)
+	fmt.Println(t2)
+	fmt.Println(err)
+}
