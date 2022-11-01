be50489db1288c7954ceff3a1cc51bdf172ef297
João E. Pereira Jr <joaoepj@gmail.com>
2022-11-01 13:59:43 -0300

    Picking Numbers second fail

--- lg_problems/picking-numbers.go
+++ lg_problems/picking-numbers.go
@@ -1,7 +1,10 @@
 package lg_problems
 
+import "fmt"
+
 var PickingNumbers_arr = []int32{1, 1, 2, 2, 4, 4, 5, 5, 5}
 
+// first submission
 func PickingNumbers(a []int32) int32 {
 	var result int32
 	//sb stands for subarrays
@@ -17,4 +20,45 @@ func PickingNumbers(a []int32) int32 {
 	}
 	//sort.Ints(sb)
 	return result
+}
+
+func myAbs(x int32) int32 {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+// second submission
+func pickingNumbers(a []int32) int32 {
+
+	var result, start, end int32
+	var sbl []int32
+	// Write your code here
+	for {
+		// stop condition 1
+		if end == int32(len(a)-1) {
+			sbl = append(sbl, end-start+1)
+			break
+		}
+		// sub array finished
+		if myAbs(a[end+1]-a[end]) > 1 {
+			sbl = append(sbl, end-start+1)
+			start = end + 1
+			end = end + 1
+		} else {
+			// building subarray
+			end = end + 1
+		}
+	}
+	fmt.Println("sbl: ", sbl)
+	// search longest array
+	for _, v := range sbl {
+		if result < v {
+			result = v
+		}
+	}
+	// return
+	return result
+
 }
