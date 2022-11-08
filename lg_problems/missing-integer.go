c7217351b4b7868c4fe2b2438a5b00e87a6df0c1
João E. Pereira Jr <joaoepj@gmail.com>
2022-11-08 13:51:17 -0300

    Create missing-integer.go

---
+++ lg_problems/missing-integer.go
@@ -0,0 +1,54 @@
+// From https://codility.com
+package lg_problems
+
+import "fmt"
+
+// There is a little problem I didnt bother to spot
+// Maybe you are the luck one
+func MissingInteger(A []int) int {
+	var result int = -1
+	var tmp int
+	m := make(map[int]int)
+	for _, v := range A {
+		if v > 0 {
+			m[v] = v
+		}
+	}
+
+	for i := 2; i <= len(m); i++ {
+		// if 1 is not present, then it is the answer
+		if _, ok := m[1]; ok == false {
+			result = 1
+			break
+		}
+		// if v is present, it can be the last
+		// else missing integer was found
+		if v, ok := m[i]; ok == true {
+			tmp = v
+		} else {
+			result = i
+			break
+		}
+
+	}
+	// if missing integer was not found, then it is the successor of last
+	if result == -1 {
+		result = tmp + 1
+	}
+	return result
+
+}
+
+func TestMissingInteger() {
+	x := [][]int{
+		{1, 3, 6, 4, 1, 2},
+		{1, 2, 3},
+		{-1, -3},
+		{1, 3},
+		{0, 3},
+		{-5, 3},
+	}
+	for _, v := range x {
+		fmt.Println(Solution(v))
+	}
+}
