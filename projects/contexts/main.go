c03da6357e530b781eb84264fcb534d4c43ef322
Joao E Pereira Jr <joaoepj@gmail.com>
2022-11-09 16:43:05 -0300

    context.WithValue context.WithCancel. Introducing log

--- projects/contexts/main.go
+++ projects/contexts/main.go
@@ -1,15 +1,45 @@
 package main
 
 import (
-	"fmt"
 	"context"
+	"log"
+	"time"
 )
 
 func doSomething(ctx context.Context) {
-	fmt.Printf("doSomething: context's key is %s\n", ctx.Value("myKey"))
+	ctx, cancelCtx := context.WithCancel(ctx)
+	printCh := make(chan int)
+
+	go doAnother(ctx, printCh)
+
+	for i := 0; i < 5; i++ {
+		printCh <- i
+	}
+	cancelCtx()
+
+	time.Sleep(100 * time.Millisecond)
+
+	log.Println("finished")
+}
+
+func doAnother(ctx context.Context, printCh <-chan int) {
+	log.Println("context's key value is: ", ctx.Value("myKey"))
+	for {
+		select {
+		case <-ctx.Done():
+			if err := ctx.Err(); err != nil {
+				log.Printf("error: %s\n", err)
+			}
+			log.Println("finished")
+			return
+		case i := <-printCh:
+			log.Printf("%d\n", i)
+		}
+	}
 }
 
 func main() {
+	log.SetFlags(log.Lshortfile | log.Lmicroseconds | log.LstdFlags)
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, "myKey", "myValue")
 	doSomething(ctx)
